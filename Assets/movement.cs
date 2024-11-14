4fb208ad3ad1f39819347a46d7f0f81662caf9d9
majimae <martaburczyk@onet.pl>
2024-11-14 17:00:10 +0100

    4

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,52 +1,84 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
 {
-    public Rigidbody rb;
+    Rigidbody rb;
+    SpriteRenderer sr;
+    Animator anim;
 
-    Animator anim; 
-
-
-    public float upForce = 100;
-    public float speed = 1500;
-    public float runSpeed = 2500;
+    public float upForce = 50;
+    [SerializeField]public float speed = 100;
+    [SerializeField]public float runSpeed = 500;
 
     public bool isGrounded = false;
 
-
-    // Start is called before the first frame update
+    bool isLeftShift;
+    float moveHorizontal;
+    float moveVertical;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        isLeftShift = Input.GetKey(KeyCode.LeftShift);
+        //Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal > 0)
+        {
+            sr.flipX = false;
+        }
+        else if(moveHorizontal < 0)
+        {
+            sr.flipX = true;
+        }
+
+        if (moveHorizontal == 0 && moveVertical == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * upForce);
+            isGrounded = false;
+            anim.SetBool("isGrounded",false);
+            anim.SetTrigger("jump");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    
+    private void FixedUpdate()
+    {
+        if (isLeftShift)
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                rb.velocity = new Vector3(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, rb.velocity.y, 0);
-            }
-            else
-            {
-                rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y, 0);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                rb.AddForce(Vector3.up * upForce);
-                isGrounded = false;
-            }
-
+            float normalizedSpeed = runSpeed * Time.deltaTime;
+            rb.velocity = new Vector3(moveHorizontal * normalizedSpeed, rb.velocity.y,moveVertical * normalizedSpeed);
         }
-
-        private void OnCollisionEnter(Collision collision)
+        else 
         {
-            isGrounded = true;
+            float normalizedSpeed = speed * Time.deltaTime;
+            rb.velocity = new Vector3(moveHorizontal * normalizedSpeed, rb.velocity.y,moveVertical * normalizedSpeed);
         }
-    
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+        anim.SetBool("isGrounded",true);
+    }
 }
-
